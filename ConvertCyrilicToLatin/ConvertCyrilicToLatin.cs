ba0d412a4a09605cd082da6ebd3aab701f2209f4
Dražen <blanusaf1@gmail.com>
2019-04-07 23:33:15 +0200

    Resena upozorenja

--- ConvertCyrilicToLatin/ConvertCyrilicToLatin.cs
+++ ConvertCyrilicToLatin/ConvertCyrilicToLatin.cs
@@ -62,12 +62,13 @@ namespace ConvertCyrilicToLatin
         readonly string[] rus_up2 = { "А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
         readonly string[] rus_low2 = { "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
 
-        Boolean sendStatus = false;
+        bool sendStatus = false;
         StringBuilder bodyText = new StringBuilder();
         MailMessage message = new MailMessage();
         public int Width { get; set; }
         public int Height { get; set; }
         public string ImageURL { get; set; }
+        public bool SendStatus { get => sendStatus; set => sendStatus = value; }
 
         public string CyrilicToLatin(string cyrTextParam)
         {
